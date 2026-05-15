383c15415205ee0ae6ac183440f14f4d39bad534
Lemuel Aquino De Ramos <149219036+Imnotlemwel@users.noreply.github.com>
2026-05-16 01:44:16 +0800

    Add files via upload

---
+++ Quiz3/quiz3/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    # Method requirement
+    def product_info(self):
+        return f"{self.name} costs {int(self.price)}"
+
+    def __str__(self):
+        return self.name
